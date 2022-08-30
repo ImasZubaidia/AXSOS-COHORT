24c06bb7914201aee9d1a2f1aac8f3eed7ccfc87
Imas Zubaidia <imassamirammal97@gmail.com>
2022-08-30 22:10:30 +0300

    Buttonclicker

---
+++ Assignments/JS/Button Clicker/buttonclicker.js
@@ -0,0 +1,18 @@
+function Login(element) {
+(element.innerText == "Login")
+  alert("Logout")
+  element.innerText = "Logout";
+  }
+var x = 0;
+function Like(element) {x++;
+  alert("the ninja was liked" + x);
+  element.innerText == x + "Likes";
+}
+var y = 0;
+function Likes(element) {y++;
+  alert("the ninja was liked" + z);
+  element.innerText == z + "Likes";
+}
+function hide(element) {
+  element.remove();
+}
