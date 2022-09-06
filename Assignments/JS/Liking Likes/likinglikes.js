49ba53d886cded4adff76c5122ad6950b21cd009
Imas Zubaidia <imassamirammal97@gmail.com>
2022-09-07 00:52:55 +0300

    Liking Likes

---
+++ Assignments/JS/Liking Likes/likinglikes.js
@@ -0,0 +1,19 @@
+function countLikesBox1() {
+
+    var likeCountText = document.getElementById("box1-like-count").innerText;
+    var count = parseInt(likeCountText);
+    count++;
+  document.getElementById("box1-like-count").innerText = count;
+}
+function countLikesBox2() {
+  var likeCountText = document.getElementById("box2-like-count").innerText;
+  var count = parseInt(likeCountText);
+  count++;
+  document.getElementById("box2-like-count").innerText = count;
+}
+function countLikesBox3() {
+  var likeCountText = document.getElementById("box3-like-count").innerText;
+  var count = parseInt(likeCountText);
+  count++;
+  document.getElementById("box3-like-count").innerText = count;
+}
