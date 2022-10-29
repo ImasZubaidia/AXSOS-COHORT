d67642fe62075550b119f6e273e114251cd7fe60
Imas Zubaidia <imassamirammal97@gmail.com>
2022-10-29 15:52:26 +0300

    Hello

---
+++ Assignments/Java/java_fun/greetings/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        String myName ="My name is Imas Zubaidia" ;
+        String myAge = "I am 25 years old";
+        String myCountry = "My hometown is Ramallah PS";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myCountry);
+    }
+}
