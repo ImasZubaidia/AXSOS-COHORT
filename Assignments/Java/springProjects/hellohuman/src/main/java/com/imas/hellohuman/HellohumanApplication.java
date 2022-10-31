e803fdd25e2091e98d9dcb8656a1f7903f33b23f
Imas Zubaidia <imassamirammal97@gmail.com>
2022-10-31 18:00:21 +0300

    hellohuman

---
+++ Assignments/Java/springProjects/hellohuman/src/main/java/com/imas/hellohuman/HellohumanApplication.java
@@ -0,0 +1,15 @@
+package com.imas.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class HellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanApplication.class, args);
+	}
+
+}
