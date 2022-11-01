56853af1e3f81658457384adbb9e6c85ccc76c40
Imas Zubaidia <imassamirammal97@gmail.com>
2022-11-01 15:50:00 +0300

    Hopper

---
+++ Assignments/Java/springProjects/hopper/src/test/java/com/imas/hopper/HopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imas.hopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
